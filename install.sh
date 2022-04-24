68995a9a8cfeb9c3617d5f52e69a2c257ef62754
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2022-04-24 18:01:32 +0000

    install to $HOME/.local/bin

--- install.sh
+++ install.sh
@@ -4,17 +4,14 @@ pip install poetry
 
 # create ~/bin
 mkdir -p ~/bin
-
 # download and extract just to ~/bin/just
-curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ~/bin
-
+curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to $HOME/.local/bin
 
 # add `~/bin` to the paths that your shell searches for executables
 # this line should be added to your shells initialization file,
 # e.g. `~/.bashrc` or `~/.zshrc`
-export PATH="$PATH:$HOME/bin"
-
-echo 'export PATH="$PATH:$HOME/bin" ' >> $HOME/.bashrc
+# export PATH="$PATH:$HOME/bin"
+# echo 'export PATH="$PATH:$HOME/bin" ' >> $HOME/.bashrc
 
 # just should now be executable
 just --help
