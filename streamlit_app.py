2286acbca612cbf1b5a81150d1db5d32b60c64c6
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2022-04-29 10:14:22 +0300

    streamlit: IMOEX composition

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,33 @@
 import streamlit as st
 
-# """Демонстрация возможностей библиотеки finec."""
+"""Демонстрация возможностей библиотеки finec:
+
+- получить данные Московской биржи по акциям, облигациям или валютам
+- преобразовать данные или объединить с данными из других источников
+- построить графики
+- записать в Excel
+
+Все выполняется на основе [открытого кода](https://github.com/epogrebnyak/finec). 
+"""
 
 st.header("Акции")
 
+"""Какие акции входят в состав индекса Мосбиржи и с какими весами?"""
+
+from finec.moex import Index
+import pandas as pd
+
+imoex_dict = Index("IMOEX").composition()
+cols = "ticker shortnames weight".split()
+imoex_df = pd.DataFrame(imoex_dict)[cols].sort_values("weight", ascending=False).reset_index(drop=True)
+st.dataframe(imoex_df)
+
+# TODO: "Скачать данные (CSV, Excel)"
+
 st.header("Облигации")
 
-st.header("Скачать данные")
+"""По каким облигациям проходят наибольшие объемы торговли?"""
+
+st.header("Как скачать эти данные")
+
+# Create in tempdir
